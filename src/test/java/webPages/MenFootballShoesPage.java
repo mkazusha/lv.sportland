376929a5cae5eb7fe3b3fa0de65a24922d48509b
MARIA <maria.kolesnikova96@gmail.com>
2019-10-01 09:57:18 +0300

    Collected info about product and added to the txt file

--- src/test/java/webPages/MenFootballShoesPage.java
+++ src/test/java/webPages/MenFootballShoesPage.java
@@ -18,6 +18,7 @@ public class MenFootballShoesPage {
     private final By SORTING_OPTIONS = By.xpath(".//select");
     private final By FILTER_NAMES = By.xpath(".//fieldset/ol/li/label");
     private final By FILTER_CHECKBOX = By.xpath(".//input[@type='checkbox']");
+    private final By PRODUCT_INFORMATION = By.xpath(".//a[@class = 'spodb-product-card']");
 
     private BaseFunc baseFunc;
 
@@ -50,8 +51,12 @@ public class MenFootballShoesPage {
     }
 
     public void collectInformation_txt() throws IOException {
-        File file = new File("myFirstFile.txt");
+        List<WebElement> information = baseFunc.getAllElements(PRODUCT_INFORMATION);
+        File file = new File("menFootbalShoes.txt");
         FileWriter fw = new FileWriter(file);
-        fw.write("Hello");
+        for (WebElement info: information) {
+            fw.write(info.getText() + "\r\n");
+        }
+        fw.close();
     }
 }
